cadaf4ae955dbc79319b9d637d36a3813ed9a877
Zeljko Margeta <zeljko_margeta@epam.com>
2025-07-09 11:28:30 +0200

    feat: add storybook

---
+++ src/stories/Counter.stories.jsx
@@ -0,0 +1,13 @@
+import Counter from '../Counter.js'
+
+export default {
+  title: 'Counter',
+  component: Counter,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  args: {
+    initialValue: 0,
+  },
+}
---
+++ src/stories/GenreSelect.stories.js
@@ -0,0 +1,16 @@
+import GenreSelect from '../GenreSelect.jsx'
+import { fn } from 'storybook/test'
+
+export default {
+  title: 'GenreSelect',
+  component: GenreSelect,
+  tags: ['autodocs'],
+}
+
+export const Default = {
+  args: {
+    values: ['All', 'Documentary', 'Comedy', 'Horror', 'Crime'],
+    defaultValue: 'All',
+    onChange: fn(),
+  },
+}
